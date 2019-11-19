71e8774ca7f34f1e18715b42d09a500d009ea7e4
Re-z <re-z@ukr.net>
2019-11-19 20:19:41 +0200

    creating layout in progress

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="#wrap">
+        
+	</div>
+</template>
+<script>
+export default {
+	
+}
+</script>
+<style lang="scss">
+@import url('assets/css/index.css');
+@import url('~materialize-css/dist/css/materialize.min.css');
+// @import url('https://cdnjs.cloudflare.com/ajax/libs/mamterialize/1.0.0/css/materialize.min.css');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import Home from '../views/Home.vue'
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: Home
-  // }
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => {import('../views/Login.vue')}
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    component: () => {import('../views/Categories.vue')}
+  },
 ];
 
 const router = new VueRouter({
